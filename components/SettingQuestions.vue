9c1cc55e8f4d56a64a3d8844376f6658612a2d04
cedrictaro-g <cedric.tarou@gmail.com>
2022-09-28 06:24:17 +0900

    added description for adding questions

--- components/SettingQuestions.vue
+++ components/SettingQuestions.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
     <div class="mt-3">
+      <!-- 説明部分削除できるにようにする -->
+      <div class="description grey pa-2 rounded-lg mb-3">
+        <p class="white--text">
+          問題の登録を行ってください。<br />
+          <span><v-icon>mdi-plus-circle-outline</v-icon></span
+          >を押すと問題が追加されます。<br />
+          <span><v-icon>mdi-trash-can-outline</v-icon></span
+          >を押すとすべての問題がリセットされます。<br />
+          正答数を基準に採点するので、問題数と配点、観点をそれぞれ設定してください。<br />
+          合計点数が100を超えるとエラーメッセージが表示されます。<br />
+          問題の設定が完了したら、上の<span class="font-weight-bold"
+            >生徒登録のタブ</span
+          >から生徒の登録を行ってください。
+        </p>
+      </div>
       <div class="btn-group">
         <v-btn color="success" @click="addQuestion">
           <v-icon>mdi-plus-circle-outline</v-icon>追加する</v-btn
@@ -162,3 +177,5 @@ export default {
   },
 }
 </script>
+<style lang="scss" scoped>
+</style>
